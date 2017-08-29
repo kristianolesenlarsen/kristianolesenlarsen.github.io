58a32cd1e69f2d35920e221f89238ed6128d2e5d
Kristian Urup Olesen Larsen <qsd161@SAMF.DOMAIN>
2017-08-29 16:31:13 +0200

    random tests

--- test.py
+++ test.py
@@ -1,36 +1,32 @@
-from webbrowser import open
-
-open('https://google.com')
-
-
-a = False
-b = True
-
-if not a and not b:
-    print('yes')
-else:
-    print('no')
 
 
 
-class student():
+def calculator(a, b):
+    return calcinternals(a, b)
 
-  def __init__(self, skills, ambitions):
-    self.skills = skills
-    self.ambitions = ambitions
 
-  def study(self, need_beer = True):
-    if self.ambitions > self.skills:
-        if need_beer:
-            print("I wont study today even though im behind on school, instead i'll go get a drink")
-            return False
-        else:
-            print("I'm already studying since my skills don't match my ambitions, and i dont want beer right now")
-            return True
-    else:
-        print("I have studied more than i had ambitions to do, i'll get a beer now")
-        return False
+class calcinternals():
+    def __init__(self, a, b):
+        self.A = a
+        self.B = b
 
-Allan = student(skills = 10, ambitions = 20)
+    @property
+    def sum(self):
+        return self.A + self.B
 
-Allan.study(need_beer = False)
+    @property
+    def difference(self):
+        return self.A - self.B
+
+    @property
+    def product(self):
+        return self.A * self.B
+
+    @property
+    def fraction(self):
+        return self.A / self.B
+
+
+calc = calculator(3,4)
+
+calc.difference
